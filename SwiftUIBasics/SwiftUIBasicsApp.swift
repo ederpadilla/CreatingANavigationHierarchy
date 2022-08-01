4dfbe525c0786d9fdbb3f628d31acd9bf7616272
€%er Padilla! <eder.padilla97@gmial.com>
2022-07-31 19:00:43 -0500

    Initial Commit

---
+++ SwiftUIBasics/SwiftUIBasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBasicsApp.swift
+//  SwiftUIBasics
+//
+//  Created by Eder  Padilla on 31/07/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
